617b1117d186ee0896cf7722d01f3234e7ae424b
Mircea Mare <shotgundebugging@users.noreply.github.com>
2017-12-11 09:42:23 +0200

    Symmetric backup and restore scripts

--- backup.sh
+++ backup.sh
@@ -1,21 +1,28 @@
+./restore.sh
+
+# zsh
+cp -R $HOME/.oh-my-zsh/themes/shotgundebugging.zsh-theme .
+cp -R $HOME/.zshrc .
+
+# git
 cp $HOME/.gitconfig .
 cp $HOME/.gitmessage .
 sed -i.bak '/token/d' .gitconfig
 rm .gitconfig.bak
 cp .gitconfig _gitconfig
 
-cp -R $HOME/.zshrc .
+# OS X profile
 cp -R $HOME/.profile .
 cp -R $HOME/.bash_profile .
 cp -R $HOME/.bashrc .
 cp -R $HOME/.zlogin .
 
+# atom
 mkdir atom
 cp $HOME/.atom/* ./atom/
-
-cp -R $HOME/.oh-my-zsh/themes/shotgundebugging.zsh-theme .
-cp -R $HOME/com.googlecode.iterm2.plist .
-
 find $HOME/.atom/packages -type d -maxdepth 1 | cut -f 6 -d '/' > ./atom/atom-packages.new
 cat ./atom/atom-packages ./atom/atom-packages.new | sort | uniq > ./atom/atom-packages
 rm ./atom/atom-packages.new
+
+# iterm
+cp -R $HOME/com.googlecode.iterm2.plist .
--- restore.sh
+++ restore.sh
@@ -1,6 +1,21 @@
-cp shotgundebugging.zsh-theme ~/.oh-my-zsh/themes
-cp .zshrc ~/
+# zsh
 touch ~/.secrets
-# cat ./atom/atom-packages | xargs apm install
+cp shotgundebugging.zsh-theme ~/.oh-my-zsh/themes/
+cp .zshrc ~/
+
+# git
+cp _gitconfig $HOME/.gitconfig
+cp .gitmessage $HOME/
+
+# OS X profile
+cp .profile $HOME/
+cp .bash_profile $HOME/
+cp .bashrc $HOME/
+cp .zlogin $HOME/
+
+# atom
 cp ./atom/* $HOME/.atom/
-cp .gitconfig ~/.gitconfig
+# cat ./atom/atom-packages | xargs apm install
+
+# iterm
+cp com.googlecode.iterm2.plist $HOME/
